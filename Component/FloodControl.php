94e17a8ca7ea7bd4b5b06569cc31effb610b66a6
Marcel Gläser <marcelglaeser@users.noreply.github.com>
2018-12-11 18:33:58 +0100

    Update FloodControl.php

--- Component/FloodControl.php
+++ Component/FloodControl.php
@@ -13,7 +13,7 @@
 
 namespace CCDNForum\ForumBundle\Component;
 
-use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
@@ -32,9 +32,9 @@ class FloodControl
     /**
      *
      * @access protected
-     * @var \Symfony\Component\Security\Core\SecurityContextInterface $securityContext
+     * @var \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $securityAuthorizationChecker
      */
-    protected $securityContext;
+    protected $securityAuthorizationChecker;
 
     /**
      *
@@ -67,15 +67,15 @@ class FloodControl
     /**
      *
      * @access public
-     * @param \Symfony\Component\Security\Core\SecurityContextInterface $securityContext
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $securityAuthorizationChecker
      * @param \Symfony\Component\HttpFoundation\Session\Session         $session
      * @param string                                                    $kernelEnv
      * @param int                                                       $postLimit
      * @param int                                                       $blockTimeInMinutes
      */
-    public function __construct(SecurityContextInterface $securityContext, Session $session, $kernelEnv, $postLimit, $blockTimeInMinutes)
+    public function __construct(AuthorizationCheckerInterface $securityAuthorizationChecker, Session $session, $kernelEnv, $postLimit, $blockTimeInMinutes)
     {
-        $this->securityContext = $securityContext;
+        $this->securityAuthorizationChecker = $securityAuthorizationChecker;
         $this->session = $session;
         $this->kernelEnv = $kernelEnv;
 
@@ -93,7 +93,7 @@ class FloodControl
      */
     public function incrementCounter()
     {
-        if (! $this->securityContext->isGranted('ROLE_MODERATOR') || $this->kernelEnv != 'prod') {
+        if (! $this->securityAuthorizationChecker->isGranted('ROLE_MODERATOR') || $this->kernelEnv != 'prod') {
             $postCount = $this->session->get('flood_control_forum_post_count');
 
             $postCount[] = new \DateTime('now');
@@ -109,7 +109,7 @@ class FloodControl
      */
     public function isFlooded()
     {
-        if ($this->postLimit < 1 || ! $this->securityContext->isGranted('ROLE_MODERATOR') || $this->kernelEnv != 'prod') {
+        if ($this->postLimit < 1 || ! $this->securityAuthorizationChecker->isGranted('ROLE_MODERATOR') || $this->kernelEnv != 'prod') {
             return false;
         }
 
